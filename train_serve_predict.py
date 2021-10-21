3261384ec91ca7ebd9979af17d1adc574b5e1f73
David <preti.david@gmail.com>
2021-10-21 22:44:53 +0200

    serve and predict

--- train_serve_predict.py
+++ train_serve_predict.py
@@ -4,7 +4,10 @@ from tensorflow import keras
 import numpy as np
 import argparse
 import os
+import requests
+import json
 
+#https://www.tensorflow.org/tfx/tutorials/serving/rest_simple
 
 def get_dataset():
     fashion_mnist = keras.datasets.fashion_mnist
@@ -23,14 +26,14 @@ def get_dataset():
 
     print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
     print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
-    return train_images, train_labels, test_images, test_labels
+    return train_images, train_labels, test_images, test_labels, class_names
 
 def train_model(epochs, train_images, train_labels, test_images, test_labels):
     model = keras.Sequential([
     keras.layers.Conv2D(input_shape=(28,28,1), filters=8, kernel_size=3, 
                         strides=2, activation='relu', name='Conv1'),
     keras.layers.Flatten(),
-    keras.layers.Dense(10, name='Dense')
+    keras.layers.Dense(10, name='Dense',activation='sorfmax')
     ])
     model.summary()
 
@@ -59,10 +62,19 @@ def export_model(model, model_dir, version):
     )
     print('\nSaved model: {}'.format(export_path))
 
-def start_serving(model_dir, version): 
-    export_path = os.path.join(model_dir, str(version))
+def start_serving(model_dir): 
     os.system('docker pull tensorflow/serving')
     os.system(' docker run -p 8501:8501 -v '+str(os.path.abspath(model_dir))+'/:/serve/model -e MODEL_NAME=model -e MODEL_BASE_PATH=/serve/ tensorflow/serving &') 
+
+def predict(test_images, test_labels, class_names,N):
+    data = json.dumps({"signature_name": "serving_default", "instances": test_images[0:N].tolist()})
+    headers = {"content-type": "application/json"}
+    json_response = requests.post('http://localhost:8501/v1/models/model:predict', data=data, headers=headers)
+    predictions = json.loads(json_response.text)['predictions']
+
+    for i,pred in enumerate(predictions):
+        print('Predicted {} vs {}'.format(np.argmax(pred), test_labels[i]))
+        print(predictions)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -71,11 +83,12 @@ if __name__=='__main__':
     parser.add_argument("--epoch", default=10)
     parser.add_argument("--train", action='store_true')
     parser.add_argument("--serve", action='store_true')
+    parser.add_argument("--predict", action='store_true')
     
     args = parser.parse_args()
 
+    train_images, train_labels, test_images, test_labels, class_names = get_dataset()
     if args.train:
-        train_images, train_labels, test_images, test_labels = get_dataset()
         model                     = train_model(epochs       = 10,  
                                                 train_images = train_images,
                                                 train_labels = train_labels,
@@ -85,5 +98,10 @@ if __name__=='__main__':
                     model_dir   = args.path,
                     version     = args.version)
     if args.serve: 
-        start_serving(model_dir=args.path,
-                      version = args.version)
+        start_serving(model_dir=args.path)
+
+    if args.predict: 
+        predict(test_images = test_images, 
+                test_labels = test_labels, 
+                class_names = class_names,
+                N           = 2)
